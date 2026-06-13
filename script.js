b60a4e64bb4bcf8bd7a4f5e760fb619da43472bc
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-13 17:06:16 +0100

    feat: add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+
 console.log("Hello word")
+
+// Write the logic to get the computer choice
+function getComputerChoice (){
+    // Create a random number * 3 = (0, 1, 2)
+    const randomNumber = Math.floor(Math.random() * 3);
+    // Map the random number to rock, paper, or scissors
+    if (randomNumber === 0){
+        return "rock";
+    }if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+// Test the function using 
+console.log(getComputerChoice())
