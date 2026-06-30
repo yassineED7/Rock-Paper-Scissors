9f38208f9cd70e963131475f51dd18f1c1cafc6d
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-30 17:13:08 +0100

    feat: add interactive buttons and connect DOM events to playRound

--- script.js
+++ script.js
@@ -37,26 +37,24 @@ function playGame() {
         // Make humanChoice Case-insensitivity
         humanChoice = humanChoice.toLowerCase()
 
-        // Check if human wins
-        if ((humanChoice === "rock" && computerChoice === "scissors") ||
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper")) {
-            // Check specific human choice to return the exact message
-            if (humanChoice === "rock") { return ["User WIN: Rock beats Scissors!", ++humanScore, computerScore] }
-            else if (humanChoice === "paper") { return ["User WIN: Paper beats Rock!", ++humanScore, computerScore] }
-            else { return ["User WIN: Scissors beats Paper!", ++humanScore, computerScore] }
-            // Check if computer wins
-        } else if ((humanChoice === "rock" && computerChoice === "paper") ||
-            (humanChoice === "paper" && computerChoice === "scissors") ||
-            (humanChoice === "scissors" && computerChoice === "rock")) {
-            // Check specific human choice to show how it lost
-            if (humanChoice === "rock") { return ["Computer WIN: Rock loses to Paper!", humanScore, ++computerScore] }
-            else if (humanChoice === "paper") { return ["Computer WIN: Paper loses to Scissors!", humanScore, ++computerScore] }
-            else { return ["Computer WIN: Scissors loses to Rock!", humanScore, ++computerScore] }
-            // If no one wins then it is a draw
-        } else {
+        if (humanChoice === computerChoice){
             return ["It's a Draw !!!", humanScore, computerScore]
         }
+
+        const condition = (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper"));
+
+        // Check if human wins
+        if (condition) {
+            // Check specific human choice to return the exact message
+            return ["User WIN", ++humanScore, computerScore];
+            // Check if computer wins
+        } else {
+            // Check specific human choice to show how it lost
+            return ["Computer WIN", humanScore, ++computerScore];
+        } 
 
     }
 
